1bfa64c5a3768d226eb6a7c92a9bfe24db5e4e2b
mac <mac@macdemacbook-pro.local>
2016-11-15 10:15:40 +0800

    Initial Commit

---
+++ XML/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XML
+//
+//  Created by mac on 16/11/15.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
